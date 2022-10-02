10e5eef8b5008e1b2b41b0ca6b35bf01bb6af9ac
kevs25 <kevindaniel200225@gmail.com>
2022-10-02 22:12:46 +0530

    changes made

--- model.js
+++ model.js
@@ -1,9 +1,22 @@
-import * as THREE from '/node_modules/three/build/three.module';
-import {OrbitControls} from '/node_modules/three/examples/jsm/controls/OrbitControls.js'
-import {GLTFLoader} from '/node_modules/three/examples/jsm/loaders/GLTFLoader.js';
-import background from '/images/background.jpg';
-const model1 = new URL('./3D models/P1.glb', import.meta.url);
+import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
 
+import background from './images/background.jpg';
+const model1 = new URL('./3D models/P1.glb', import.meta.url);
+/* const model2 = new URL('./3D models/P1.glb', import.meta.url);
+const model3 = new URL('./3D models/P1.glb', import.meta.url);
+const model4 = new URL('./3D models/P1.glb', import.meta.url);
+const model5 = new URL('./3D models/P1.glb', import.meta.url);
+const model6 = new URL('./3D models/P1.glb', import.meta.url);
+const model7 = new URL('./3D models/P1.glb', import.meta.url);
+const model8 = new URL('./3D models/P1.glb', import.meta.url);
+const model9 = new URL('./3D models/P1.glb', import.meta.url);
+const model10 = new URL('./3D models/P1.glb', import.meta.url);
+const model11 = new URL('./3D models/P1.glb', import.meta.url);
+const model12 = new URL('./3D models/P1.glb', import.meta.url); */
+
+var model_array = [model1,model2,model3]
 const renderer = new THREE.WebGL1Renderer();
 renderer.shadowMap.enabled = true;
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -39,19 +52,22 @@ const textureLoader = new THREE.TextureLoader();
 scene.background = textureLoader.load(background); 
 
 const loader = new GLTFLoader();
-loader.load(model1.href, function(glb){
-    var model = glb.scene;
-    model.position.set(0, -1, 0);
-    model.scale.set(82,82,82);
-    model.rotation.y = -0.5 * Math.PI
-    scene.add(model);
-}, undefined, function(error) {
-    console.error(error);
-});
-function animate () {
-    renderer.render(scene, camera);
+for(var i=0; i<=model_array.length; i++){
+    loader.load('./3D models/P1/glb', function(glb){
+        var model = glb.scene;
+        model.position.set(0, -1, 0);
+        model.scale.set(82,82,82);
+        model.rotation.y = -0.5 * Math.PI
+        scene.add(model);
+    }, undefined, function(error) {
+        console.error(error);
+    });
+    function animate () {
+        renderer.render(scene, camera);
+    }
+    renderer.setAnimationLoop(animate);
 }
-renderer.setAnimationLoop(animate);
+
 
 window.addEventListener('resize', function() {
     camera.aspect = window.innerWidth / this.window.innerHeight;
